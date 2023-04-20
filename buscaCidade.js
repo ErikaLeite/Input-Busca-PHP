087314af93436664d32d02d7c901147b14e3df40
Erika Leite <71355947+ErikaLeite@users.noreply.github.com>
2023-04-20 16:31:09 -0300

    Create buscaCidade.js

---
+++ buscaCidade.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    //quando o campo codcidade perder a seleção ativa a função
+    $("input[name='codCidade']").blur(function() {
+        //declaramos que a variavel corresponde ao campo cidade
+        var $cidade = $("input[name='cidade']");
+        //declaramos que a variavel codigo recebe o valor inserido no campo
+        var codigo  = $(this).val();
+
+        //coletamos o retorno do arquivo de busca, o valor do campo
+        $.getJSON('busca.php', {codigo},
+            //e dispara a função que preenche o campo
+            function(retorno) {
+                //insere o valor recebido na posição .cidade
+                $cidade.val(retorno.cidade);
+            }
+        );
+    });
+});
